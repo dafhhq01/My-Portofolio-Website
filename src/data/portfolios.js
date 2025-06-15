49aca51f1634435b4797bedc74894fc91413f954
dafhhq01 <dafhhq01@gmail.com>
2025-06-15 12:44:13 +0700

    Update project

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,38 +1,16 @@
+import invitation from '../assets/images/Invitation-Website.png';
+import companyprofile from '../assets/images/Company-Profile.png';
 
 const portfolios = [
   {
     title: "Invitation Website",
-    image: "/src/assets/images/blue-car.jpg",
+    image: invitation,
     description:
       "Website undangan ini dibuat dengan desain unik dan pengalaman interaktif, memudahkan tamu RSVP dan menyimpan kenangan selamanya."
   },
   {
     title: "Company Profile",
-    image: "/src/assets/images/blue-car.jpg",
-    description:
-      "Website ini bukan hanya menampilkan informasi, tapi memperkuat citra bisnis, mempermudah komunikasi, dan meningkatkan kepercayaan mitra."
-  },
-  {
-    title: "Company Profile",
-    image: "/src/assets/images/blue-car.jpg",
-    description:
-      "Website ini bukan hanya menampilkan informasi, tapi memperkuat citra bisnis, mempermudah komunikasi, dan meningkatkan kepercayaan mitra."
-  },
-  {
-    title: "Company Profile",
-    image: "/src/assets/images/blue-car.jpg",
-    description:
-      "Website ini bukan hanya menampilkan informasi, tapi memperkuat citra bisnis, mempermudah komunikasi, dan meningkatkan kepercayaan mitra."
-  },
-  {
-    title: "Company Profile",
-    image: "/src/assets/images/blue-car.jpg",
-    description:
-      "Website ini bukan hanya menampilkan informasi, tapi memperkuat citra bisnis, mempermudah komunikasi, dan meningkatkan kepercayaan mitra."
-  },
-  {
-    title: "Company Profile",
-    image: "/src/assets/images/blue-car.jpg",
+    image: companyprofile,
     description:
       "Website ini bukan hanya menampilkan informasi, tapi memperkuat citra bisnis, mempermudah komunikasi, dan meningkatkan kepercayaan mitra."
   }
